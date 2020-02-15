ba862ff8ab5dc726d8af8e8432765461e342a70c
takeyabu <tk885130@gmail.com>
2020-02-15 16:50:50 +0900

    test3

---
+++ callback.php
@@ -0,0 +1,24 @@
+<?php
+    // Composerでインストールしたライブラリを一括読み込み
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    // アクセストークンを使いCurlHTTPClientをインスタンス化
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('/dS99PmL9r96rJ3BbmRAYktUDbUSYdBDWGa+/IMYQLvXfvx56/c3ss6jKAv36H8D1Tgo03mP7LzN87umgVZbWYi4xbNkME6Zaxy9BPLnq/DjA9VT/tDDFS748H/7PBhTcdJef79+P5pPyGP7/YL1HAdB04t89/1O/w1cDnyilFU=');
+
+    //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '
+b30cc034985a8255652f4ec0f2d758c0
+']);
+
+    // LINE Messaging APIがリクエストに付与した署名を取得
+    $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+    //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+    foreach ($events as $event) {
+        // メッセージを返信
+        $response = $bot->replyMessage(
+            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
+        );
+    }
