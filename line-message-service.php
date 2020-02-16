2042f231bed87f87307695b7460ea683bac9e1f4
takeyabu <tk885130@gmail.com>
2020-02-16 18:43:32 +0900

    test132

--- line-message-service.php
+++ line-message-service.php
@@ -43,7 +43,7 @@ class LineMessageService {
       }
       $contents = ["type"=>"carousel","contents"=>$carouselItem];
       //20200216 この配列の三番目の要素があると返ってこない（文字列でも）
-      return ['type'=>'flex', 'altText'=>'search', 'contents'=>$contents];
+      return ['type'=>'flex', 'altText'=>'search', 'contents'=>'a'];
 
 
   }
