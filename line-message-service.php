3391b08db03cf979bd8eb5da02fe6e11b6510d86
takeyabu <tk885130@gmail.com>
2020-02-16 19:11:31 +0900

    test136

--- line-message-service.php
+++ line-message-service.php
@@ -235,169 +235,169 @@ class LineMessageService {
 
    // $ratingInt = round($taberogu->{'rating'});
    // $distance = round($taberogu->{'distance'}*1000);
-   return  [
-     "type"=> "bubble",
-     "hero"=> [
-       "type"=> "image",
-       "url"=> 'aaa',
-       "size"=> "full",
-       "aspectRatio"=> "20:13",
-       "aspectMode"=> "cover",
-       "action"=> [
-         "type"=> "uri",
-         "uri"=> 'aaa'
-       ]
-     ],
-     "body"=> [
-       "type"=> "box",
-       "layout"=> "vertical",
-       "contents"=> [
-         [
-           "type"=> "text",
-           "text"=> 'bbb',
-           "weight"=> "bold",
-           "size"=> "xl"
-         ],
-         // [
-         //   "type"=> "box",
-         //   "layout"=> "baseline",
-         //   "margin"=> "md",
-         //   "contents"=> [
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 1)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 2)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 3)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 4)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 5)
-         //     ],
-         //     [
-         //       "type"=> "text",
-         //       "text"=> $taberogu->{'rating'},
-         //       "size"=> "sm",
-         //       "color"=> "#999999",
-         //       "margin"=> "md",
-         //       "flex"=> 0
-         //     ]
-         //   ]
-         // ],
-         [
-           "type"=> "box",
-           "layout"=> "vertical",
-           "margin"=> "lg",
-           "spacing"=> "sm",
-           "contents"=> [
-             // [
-             //   "type"=> "box",
-             //   "layout"=> "baseline",
-             //   "spacing"=> "sm",
-             //   "contents"=> [
-             //     [
-             //       "type"=> "text",
-             //       "text"=> "種類",
-             //       "color"=> "#aaaaaa",
-             //       "size"=> "sm",
-             //       "flex"=> 1
-             //     ],
-             //     [
-             //       "type"=> "text",
-             //       "text"=> $taberogu->{'service'},
-             //       "wrap"=> true,
-             //       "color"=> "#666666",
-             //       "size"=> "sm",
-             //       "flex"=> 5
-             //     ]
-             //   ]
-             // ],
-             [
-               "type"=> "box",
-               "layout"=> "baseline",
-               "spacing"=> "sm",
-               "contents"=> [
-                 [
-                   "type"=> "text",
-                   "text"=> "場所",
-                   "color"=> "#aaaaaa",
-                   "size"=> "sm",
-                   "flex"=> 1
-                 ],
-                 [
-                   "type"=> "text",
-                   // "text"=> $taberogu->{'street'}.' ('.$distance.'m)',
-                   "text"=> 'asjh',
-                   "wrap"=> true,
-                   "color"=> "#666666",
-                   "size"=> "sm",
-                   "flex"=> 5
-                 ]
-               ]
-             ]
-             // [ 仕様変更により取得できなくなったので閉じる
-             //   "type"=> "box",
-             //   "layout"=> "baseline",
-             //   "spacing"=> "sm",
-             //   "contents"=> [
-             //     [
-             //       "type"=> "text",
-             //       "text"=> "金額",
-             //       "color"=> "#aaaaaa",
-             //       "size"=> "sm",
-             //       "flex"=> 1
-             //     ],
-             //     [
-             //       "type"=> "text",
-             //       "text"=> $taberogu->{'price'},
-             //       "wrap"=> true,
-             //       "color"=> "#666666",
-             //       "size"=> "sm",
-             //       "flex"=> 5
-             //     ]
-             //   ]
-             // ]
-           ]
-         ]
-       ]
-     ],
-     "footer"=> [
-       "type"=> "box",
-       "layout"=> "vertical",
-       "spacing"=> "sm",
-       "contents"=> [
-         [
-           "type"=> "button",
-           "style"=> "link",
-           "height"=> "sm",
-           "action"=> [
-             "type"=> "uri",
-             "label"=> "ぐるナビをみる",
-             "uri"=> 'fd'
-           ]
-         ],
-         [
-           "type"=> "spacer",
-           "size"=> "sm"
-         ]
-       ],
-       "flex"=> 0
-     ]
-   ];
+   // return  [
+   //   "type"=> "bubble",
+   //   "hero"=> [
+   //     "type"=> "image",
+   //     "url"=> 'aaa',
+   //     "size"=> "full",
+   //     "aspectRatio"=> "20:13",
+   //     "aspectMode"=> "cover",
+   //     "action"=> [
+   //       "type"=> "uri",
+   //       "uri"=> 'aaa'
+   //     ]
+   //   ],
+   //   "body"=> [
+   //     "type"=> "box",
+   //     "layout"=> "vertical",
+   //     "contents"=> [
+   //       [
+   //         "type"=> "text",
+   //         "text"=> 'bbb',
+   //         "weight"=> "bold",
+   //         "size"=> "xl"
+   //       ],
+   //       // [
+   //       //   "type"=> "box",
+   //       //   "layout"=> "baseline",
+   //       //   "margin"=> "md",
+   //       //   "contents"=> [
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 1)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 2)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 3)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 4)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 5)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "text",
+   //       //       "text"=> $taberogu->{'rating'},
+   //       //       "size"=> "sm",
+   //       //       "color"=> "#999999",
+   //       //       "margin"=> "md",
+   //       //       "flex"=> 0
+   //       //     ]
+   //       //   ]
+   //       // ],
+   //       [
+   //         "type"=> "box",
+   //         "layout"=> "vertical",
+   //         "margin"=> "lg",
+   //         "spacing"=> "sm",
+   //         "contents"=> [
+   //           // [
+   //           //   "type"=> "box",
+   //           //   "layout"=> "baseline",
+   //           //   "spacing"=> "sm",
+   //           //   "contents"=> [
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> "種類",
+   //           //       "color"=> "#aaaaaa",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 1
+   //           //     ],
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> $taberogu->{'service'},
+   //           //       "wrap"=> true,
+   //           //       "color"=> "#666666",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 5
+   //           //     ]
+   //           //   ]
+   //           // ],
+   //           [
+   //             "type"=> "box",
+   //             "layout"=> "baseline",
+   //             "spacing"=> "sm",
+   //             "contents"=> [
+   //               [
+   //                 "type"=> "text",
+   //                 "text"=> "場所",
+   //                 "color"=> "#aaaaaa",
+   //                 "size"=> "sm",
+   //                 "flex"=> 1
+   //               ],
+   //               [
+   //                 "type"=> "text",
+   //                 // "text"=> $taberogu->{'street'}.' ('.$distance.'m)',
+   //                 "text"=> 'asjh',
+   //                 "wrap"=> true,
+   //                 "color"=> "#666666",
+   //                 "size"=> "sm",
+   //                 "flex"=> 5
+   //               ]
+   //             ]
+   //           ]
+   //           // [ 仕様変更により取得できなくなったので閉じる
+   //           //   "type"=> "box",
+   //           //   "layout"=> "baseline",
+   //           //   "spacing"=> "sm",
+   //           //   "contents"=> [
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> "金額",
+   //           //       "color"=> "#aaaaaa",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 1
+   //           //     ],
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> $taberogu->{'price'},
+   //           //       "wrap"=> true,
+   //           //       "color"=> "#666666",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 5
+   //           //     ]
+   //           //   ]
+   //           // ]
+   //         ]
+   //       ]
+   //     ]
+   //   ],
+   //   "footer"=> [
+   //     "type"=> "box",
+   //     "layout"=> "vertical",
+   //     "spacing"=> "sm",
+   //     "contents"=> [
+   //       [
+   //         "type"=> "button",
+   //         "style"=> "link",
+   //         "height"=> "sm",
+   //         "action"=> [
+   //           "type"=> "uri",
+   //           "label"=> "ぐるナビをみる",
+   //           "uri"=> 'fd'
+   //         ]
+   //       ],
+   //       [
+   //         "type"=> "spacer",
+   //         "size"=> "sm"
+   //       ]
+   //     ],
+   //     "flex"=> 0
+   //   ]
+   // ];
   }
 
 }
