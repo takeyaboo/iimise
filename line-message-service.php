41d4e8ce692be4999a694be17527e886e6cbbfe6
takeyabu <tk885130@gmail.com>
2020-02-16 17:34:05 +0900

    test107

--- line-message-service.php
+++ line-message-service.php
@@ -26,7 +26,7 @@ class LineMessageService {
     curl_setopt($conn, CURLOPT_POST, true);
     curl_setopt($conn, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$this->accessToken,'Content-type: application/json'));
     curl_setopt($conn, CURLOPT_URL,  'https://api.line.me/v2/bot/message/reply');
-    // curl_setopt($conn, CURLOPT_POSTFIELDS, $jsonParam);
+    curl_setopt($conn, CURLOPT_POSTFIELDS, $jsonParam);
 
     $result = curl_exec($conn);
 
@@ -66,158 +66,158 @@ class LineMessageService {
        "action"=> [
          "type"=> "uri",
          "uri"=> 'aaa'
-       ]
-     ],
-     "body"=> [
-       "type"=> "box",
-       "layout"=> "vertical",
-       "contents"=> [
-         [
-           "type"=> "text",
-           "text"=> 'bbb',
-           "weight"=> "bold",
-           "size"=> "xl"
-         ],
-         // [
-         //   "type"=> "box",
-         //   "layout"=> "baseline",
-         //   "margin"=> "md",
-         //   "contents"=> [
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 1)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 2)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 3)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 4)
-         //     ],
-         //     [
-         //       "type"=> "icon",
-         //       "size"=> "sm",
-         //       "url"=> $this->getStarImg($ratingInt, 5)
-         //     ],
-         //     [
-         //       "type"=> "text",
-         //       "text"=> $taberogu->{'rating'},
-         //       "size"=> "sm",
-         //       "color"=> "#999999",
-         //       "margin"=> "md",
-         //       "flex"=> 0
-         //     ]
-         //   ]
-         // ],
-         [
-           "type"=> "box",
-           "layout"=> "vertical",
-           "margin"=> "lg",
-           "spacing"=> "sm",
-           "contents"=> [
-             // [
-             //   "type"=> "box",
-             //   "layout"=> "baseline",
-             //   "spacing"=> "sm",
-             //   "contents"=> [
-             //     [
-             //       "type"=> "text",
-             //       "text"=> "種類",
-             //       "color"=> "#aaaaaa",
-             //       "size"=> "sm",
-             //       "flex"=> 1
-             //     ],
-             //     [
-             //       "type"=> "text",
-             //       "text"=> $taberogu->{'service'},
-             //       "wrap"=> true,
-             //       "color"=> "#666666",
-             //       "size"=> "sm",
-             //       "flex"=> 5
-             //     ]
-             //   ]
-             // ],
-             [
-               "type"=> "box",
-               "layout"=> "baseline",
-               "spacing"=> "sm",
-               "contents"=> [
-                 [
-                   "type"=> "text",
-                   "text"=> "場所",
-                   "color"=> "#aaaaaa",
-                   "size"=> "sm",
-                   "flex"=> 1
-                 ],
-                 [
-                   "type"=> "text",
-                   // "text"=> $taberogu->{'street'}.' ('.$distance.'m)',
-                   "text"=> 'asjh',
-                   "wrap"=> true,
-                   "color"=> "#666666",
-                   "size"=> "sm",
-                   "flex"=> 5
-                 ]
-               ]
-             ]
-             // [ 仕様変更により取得できなくなったので閉じる
-             //   "type"=> "box",
-             //   "layout"=> "baseline",
-             //   "spacing"=> "sm",
-             //   "contents"=> [
-             //     [
-             //       "type"=> "text",
-             //       "text"=> "金額",
-             //       "color"=> "#aaaaaa",
-             //       "size"=> "sm",
-             //       "flex"=> 1
-             //     ],
-             //     [
-             //       "type"=> "text",
-             //       "text"=> $taberogu->{'price'},
-             //       "wrap"=> true,
-             //       "color"=> "#666666",
-             //       "size"=> "sm",
-             //       "flex"=> 5
-             //     ]
-             //   ]
-             // ]
-           ]
-         ]
-       ]
-     ],
-     "footer"=> [
-       "type"=> "box",
-       "layout"=> "vertical",
-       "spacing"=> "sm",
-       "contents"=> [
-         [
-           "type"=> "button",
-           "style"=> "link",
-           "height"=> "sm",
-           "action"=> [
-             "type"=> "uri",
-             "label"=> "ぐるナビをみる",
-             "uri"=> 'fd'
-           ]
-         ],
-         [
-           "type"=> "spacer",
-           "size"=> "sm"
-         ]
-       ],
-       "flex"=> 0
-     ]
-   ];
+       ];
+   //   ],
+   //   "body"=> [
+   //     "type"=> "box",
+   //     "layout"=> "vertical",
+   //     "contents"=> [
+   //       [
+   //         "type"=> "text",
+   //         "text"=> 'bbb',
+   //         "weight"=> "bold",
+   //         "size"=> "xl"
+   //       ],
+   //       // [
+   //       //   "type"=> "box",
+   //       //   "layout"=> "baseline",
+   //       //   "margin"=> "md",
+   //       //   "contents"=> [
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 1)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 2)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 3)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 4)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "icon",
+   //       //       "size"=> "sm",
+   //       //       "url"=> $this->getStarImg($ratingInt, 5)
+   //       //     ],
+   //       //     [
+   //       //       "type"=> "text",
+   //       //       "text"=> $taberogu->{'rating'},
+   //       //       "size"=> "sm",
+   //       //       "color"=> "#999999",
+   //       //       "margin"=> "md",
+   //       //       "flex"=> 0
+   //       //     ]
+   //       //   ]
+   //       // ],
+   //       [
+   //         "type"=> "box",
+   //         "layout"=> "vertical",
+   //         "margin"=> "lg",
+   //         "spacing"=> "sm",
+   //         "contents"=> [
+   //           // [
+   //           //   "type"=> "box",
+   //           //   "layout"=> "baseline",
+   //           //   "spacing"=> "sm",
+   //           //   "contents"=> [
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> "種類",
+   //           //       "color"=> "#aaaaaa",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 1
+   //           //     ],
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> $taberogu->{'service'},
+   //           //       "wrap"=> true,
+   //           //       "color"=> "#666666",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 5
+   //           //     ]
+   //           //   ]
+   //           // ],
+   //           [
+   //             "type"=> "box",
+   //             "layout"=> "baseline",
+   //             "spacing"=> "sm",
+   //             "contents"=> [
+   //               [
+   //                 "type"=> "text",
+   //                 "text"=> "場所",
+   //                 "color"=> "#aaaaaa",
+   //                 "size"=> "sm",
+   //                 "flex"=> 1
+   //               ],
+   //               [
+   //                 "type"=> "text",
+   //                 // "text"=> $taberogu->{'street'}.' ('.$distance.'m)',
+   //                 "text"=> 'asjh',
+   //                 "wrap"=> true,
+   //                 "color"=> "#666666",
+   //                 "size"=> "sm",
+   //                 "flex"=> 5
+   //               ]
+   //             ]
+   //           ]
+   //           // [ 仕様変更により取得できなくなったので閉じる
+   //           //   "type"=> "box",
+   //           //   "layout"=> "baseline",
+   //           //   "spacing"=> "sm",
+   //           //   "contents"=> [
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> "金額",
+   //           //       "color"=> "#aaaaaa",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 1
+   //           //     ],
+   //           //     [
+   //           //       "type"=> "text",
+   //           //       "text"=> $taberogu->{'price'},
+   //           //       "wrap"=> true,
+   //           //       "color"=> "#666666",
+   //           //       "size"=> "sm",
+   //           //       "flex"=> 5
+   //           //     ]
+   //           //   ]
+   //           // ]
+   //         ]
+   //       ]
+   //     ]
+   //   ],
+   //   "footer"=> [
+   //     "type"=> "box",
+   //     "layout"=> "vertical",
+   //     "spacing"=> "sm",
+   //     "contents"=> [
+   //       [
+   //         "type"=> "button",
+   //         "style"=> "link",
+   //         "height"=> "sm",
+   //         "action"=> [
+   //           "type"=> "uri",
+   //           "label"=> "ぐるナビをみる",
+   //           "uri"=> 'fd'
+   //         ]
+   //       ],
+   //       [
+   //         "type"=> "spacer",
+   //         "size"=> "sm"
+   //       ]
+   //     ],
+   //     "flex"=> 0
+   //   ]
+   // ];
   }
 
 }
