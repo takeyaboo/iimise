e28ea0485b8ee8e4db9b271901fd95a1f5493723
takeyabu <tk885130@gmail.com>
2020-02-16 12:37:46 +0900

    test73

--- bot.php
+++ bot.php
@@ -165,7 +165,7 @@ function searchGoogleGeocodingAPI($address) {
 
        // $res = $lineService->postFlexMessage($eventData->getReplyToken(), $taberoguList);
        // $res = serialize($taberoguList);
-       $res = implode(",", $taberoguList);
+       $res = implode(",", $taberoguList[0]);
        $bot->replyText($eventData->getReplyToken(), $res);
      }
   }
