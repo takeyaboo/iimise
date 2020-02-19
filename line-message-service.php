453ac6eb9c497265416759d2433ba786a0073d6d
takeyabu <tk885130@gmail.com>
2020-02-20 01:41:08 +0900

    test169

--- line-message-service.php
+++ line-message-service.php
@@ -15,8 +15,8 @@ class LineMessageService {
 
   private function createJsonParameter($token, $list) {
     $messages = $this->generateFlexMessageContents($list);
-    $result = ['replyToken'=>$token, 'messages'=>$messages];
-    return json_encode($token);
+    $result = ['replyToken'=>$token, 'messages'=>$messages, 'a'=>'a']
+    return json_encode($result);
   }
 
   private function postMessage($jsonParam) {
