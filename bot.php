37de5d791d1240cb37ce13dcfb099b3a27120ac2
takeyabu <tk885130@gmail.com>
2020-02-20 01:52:26 +0900

    test171

--- bot.php
+++ bot.php
@@ -136,7 +136,9 @@ function searchGoogleGeocodingAPI($address) {
 
        $shop_detail = array(array('name' => '', 'url' => '', 'address' => ''));
        $i = 0;
+       $shop_detailes = "";
        foreach ($taberoguList['rest'] as $shop) {
+                   $shop_detailes .= '<br>';
                    //APIから取得した情報を変数に格納
                    $shop_id = empty($shop['id']) ? '' : $shop['id'];
                    $name           = empty($shop['name']) ? '' : $shop['name'];
@@ -169,12 +171,17 @@ function searchGoogleGeocodingAPI($address) {
                    $shop_detail[$i]['url'] = $url;
                    $shop_detail[$i]['address'] = $address;
 
+                   $shop_detailes .= '店名:'$name.'URL:'.$url.'住所:'.$address;
+
+
+
                    $i++;
 
                }
 
        // $res = $lineService->postFlexMessage($eventData->getReplyToken(), $taberoguList['rest']);
-       $res = $lineService->postFlexMessage($eventData->getReplyToken(), $shop_detail);
+       // $res = $lineService->postFlexMessage($eventData->getReplyToken(), $shop_detail);
+       $res = $shop_detailes;
        // $res = serialize($taberoguList);
        // $res = implode(",", array_keys($taberoguList));
        // $res = implode(",", array_keys($taberoguList));
